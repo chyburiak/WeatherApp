b4382900aab7673c19e06a6164a794214c8987fc
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2025-04-16 18:32:18 +0100

    WeatherContext added.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
+import { WeatherProvider } from './context/WeatherContext';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <WeatherProvider>
+            <App />
+        </WeatherProvider>
+    </StrictMode>,
 )
