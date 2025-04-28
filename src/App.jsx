7bb24dbd3e1e576186be69b5f397d8ce9417434c
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2025-04-28 15:00:31 +0100

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Search from './components/search/search';
 
 export default function App() {
     return (
         <div className="h-max bg-white p-4 flex flex-col items-center gap-4 m-auto container">
-
+            <Search />
         </div>
     );
 }
