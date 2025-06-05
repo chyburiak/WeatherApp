7672625a7781b01bd1aff5538309908d7cdbabc2
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2025-06-05 17:26:34 +0100

    Search Component updated. Main wrapper updated.

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -28,20 +28,25 @@ const Search = () => {
     }
 
     return (
-        <div className="relative w-sm flex flex-col gap-2">
+        <form className="relative w-full sm:w-sm flex flex-col gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+
             <input
                 type="search"
                 value={query}
                 onChange={handleChange}
                 placeholder="Find your city"
-                className="appearance-none text-2xl border rounded-xl w-full outline-none py-2 px-4"
+                className='w-full text-base sm:text-2xl py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600'
             />
 
             {cities?.data && cities.data.length > 0 && query.length !== 0 && (
-                <ul className="absolute bg-white w-full text-xl border rounded-xl mt-16 overflow-y-auto max-h-[300px] z-10">
+                <ul className="absolute bg-white w-full text-base sm:text-xl border border-indigo-600 rounded-md mt-16 overflow-y-auto max-h-[300px] z-10">
                     {cities.data.map((city) => (
                         <li
                             onClick={() => onSelectCity(city)}
+                            key={city.id}
                             className="py-2 px-4 hover:bg-gray-300 cursor-pointer"
                         >
                             {city.name}, <span className="text-gray-500"> {city.country}</span>
@@ -49,7 +54,7 @@ const Search = () => {
                     ))}
                 </ul>
             )}
-        </div>
+        </form>
     );
 };
 
