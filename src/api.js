8b4234be548661b9d0a22337c28f3f15ecd880ae
Stanislav Chyburiak <stanislav.chyburiak@gmail.com>
2025-06-10 01:46:27 +0100

    api.js updated

--- src/api.js
+++ src/api.js
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEODB_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
